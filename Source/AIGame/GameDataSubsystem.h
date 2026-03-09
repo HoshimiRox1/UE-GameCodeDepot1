43168105568dbab99e785e14bdb141d0b6eba045
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-09 13:14:43 +0800

    游戏数据实例以及日程任务数据资产

---
+++ Source/AIGame/GameDataSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameDataSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIGAME_API UGameDataSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AIGame/ScheduleTaskDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ScheduleTaskDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIGAME_API UScheduleTaskDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};

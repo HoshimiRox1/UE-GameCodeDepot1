5bdc5dc93283a0d9f9756c8e52b627fe8883e603
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-09 13:14:16 +0800

    项目初始化

---
+++ Source/AIGame/AIGame.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AIGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AIGame, "AIGame" );
+
+DEFINE_LOG_CATEGORY(LogAIGame)
+ 

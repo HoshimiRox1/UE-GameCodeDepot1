041e1b951cd81521410f00fca0e7c7ed7757ace7
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-10 12:34:25 +0800

    日程主界面Widget，主要完善NativeConstruct与HandleAttributeChanged订阅回调函数，并绑定属性条

---
+++ Source/AIGame/ScheduleMainWidget.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScheduleMainWidget.h"
+#include "StatBarWidget.h"
+#include "GameDataSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void UScheduleMainWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	// 获取GameInstanceSubsystem
+	if (UGameInstance* GameInstance = GetGameInstance())
+	{
+		TObjectPtr<UGameDataSubsystem> DataSubsystem = GameInstance->GetSubsystem<UGameDataSubsystem>();
+		
+		// 订阅GameInstanceSubsystem的广播，属性改变时调用HandleAttributeChanged
+		if (DataSubsystem)
+		{
+			DataSubsystem->OnAttributeChanged.AddDynamic(this, &UScheduleMainWidget::HandleAttributeChanged);			
+			
+			StatBar_Decision->OnValueUpdate(DataSubsystem->GetAttributeValue(EPlayerAttribute::Decision));
+			StatBar_Social->OnValueUpdate(DataSubsystem->GetAttributeValue(EPlayerAttribute::Social));
+			StatBar_Empathy->OnValueUpdate(DataSubsystem->GetAttributeValue(EPlayerAttribute::Empathy));
+		}
+	}
+}
+
+void UScheduleMainWidget::HandleAttributeChanged(EPlayerAttribute AttributeType, float NewValue)
+{
+	switch (AttributeType)
+	{
+		case EPlayerAttribute::Decision:
+			StatBar_Decision->OnValueUpdate(NewValue);
+			break;
+		case EPlayerAttribute::Social:
+			StatBar_Social->OnValueUpdate(NewValue);
+			break;
+		case EPlayerAttribute::Empathy:
+			StatBar_Empathy->OnValueUpdate(NewValue);
+			break;
+	}
+}
+
+
---
+++ Source/AIGame/ScheduleMainWidget.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ScheduleMainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIGAME_API UScheduleMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	// UI的BeginPlay()
+	virtual void NativeConstruct() override;
+	
+	// Subsystem广播后的触发函数
+	UFUNCTION()
+	void HandleAttributeChanged(EPlayerAttribute AttributeType, float NewValue);
+	
+	// 绑定三个子属性条
+	UPROPERTY(meta = (BindWidget))
+	class UStatBarWidget* StatBar_Decision;
+	
+	UPROPERTY(meta = (BindWidget))
+	class UStatBarWidget* StatBar_Social;
+	
+	UPROPERTY(meta = (BindWidget))
+	class UStatBarWidget* StatBar_Empathy;
+};

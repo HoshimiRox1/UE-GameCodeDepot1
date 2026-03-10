b7427a0f0703f34d08274b97234bdbe629563bbf
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-10 12:29:47 +0800

    角色基本属性的修改逻辑和事件监听

--- Source/AIGame/GameDataSubsystem.cpp
+++ Source/AIGame/GameDataSubsystem.cpp
@@ -3,3 +3,42 @@
 
 #include "GameDataSubsystem.h"
 
+void UGameDataSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	// 初始化默认属性数值
+	PlayerStats.Add(EPlayerAttribute::Decision, 0.0f);
+	PlayerStats.Add(EPlayerAttribute::Social, 0.0f);
+	PlayerStats.Add(EPlayerAttribute::Empathy, 0.0f);
+	PlayerStats.Add(EPlayerAttribute::Cash, 100.0f);
+	PlayerStats.Add(EPlayerAttribute::DailyMood, 1.0f);
+}
+
+void UGameDataSubsystem::ModifyAttribute(EPlayerAttribute Type, float Amount)
+{
+	if (PlayerStats.Contains(Type))
+	{
+		PlayerStats[Type] += Amount;
+		
+		// 将心情值钳制在0-1
+		if (Type == EPlayerAttribute::DailyMood)
+		{
+			PlayerStats[Type] = FMath::Clamp(PlayerStats[Type], 0.0f, 1.0f);
+		}
+		
+		// 广播属性修改
+		OnAttributeChanged.Broadcast(Type, PlayerStats[Type]);
+	}
+}
+
+void UGameDataSubsystem::ResetDailyStats()
+{
+	ModifyAttribute(EPlayerAttribute::DailyMood, 1.0f - PlayerStats[EPlayerAttribute::DailyMood]);
+	
+}
+
+float UGameDataSubsystem::GetAttributeValue(EPlayerAttribute Attribute) const
+{
+	return PlayerStats.Contains(Attribute) ? PlayerStats[Attribute] : 0.0f;
+}
--- Source/AIGame/GameDataSubsystem.h
+++ Source/AIGame/GameDataSubsystem.h
@@ -4,14 +4,42 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+#include "GameTypes.h"
 #include "GameDataSubsystem.generated.h"
 
 /**
- * 
+ 游戏单例
  */
+
+// 动态多播：属性变动->UI刷新
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAttributeChanged, EPlayerAttribute, Attribute, float, Value);
+
 UCLASS()
 class AIGAME_API UGameDataSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+public:
+	// 初始化数据
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	
+	// 修改属性的通用接口
+	UFUNCTION(BlueprintCallable, Category = "游戏属性")
+	void ModifyAttribute(EPlayerAttribute Type, float Amount);
+	
+	// 每日刷新逻辑
+	UFUNCTION(BlueprintCallable, Category = "游戏属性")
+	void ResetDailyStats();
+	
+	// 属性Getter函数
+	UFUNCTION(BlueprintPure, Category = "游戏属性")
+	float GetAttributeValue(EPlayerAttribute Attribute) const;
+	
+	// UI监听变量
+	UPROPERTY(BlueprintAssignable, Category = "事件")
+	FOnAttributeChanged OnAttributeChanged;
+	
+private:
+	// 数据池
+	UPROPERTY()
+	TMap<EPlayerAttribute, float> PlayerStats;
 };

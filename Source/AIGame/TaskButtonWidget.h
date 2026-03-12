ec95ad25f9048faec441c01b4f3af4b6146ff8d0
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-12 20:07:58 +0800

    创建任务按钮类

---
+++ Source/AIGame/TaskButtonWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TaskButtonWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIGAME_API UTaskButtonWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};

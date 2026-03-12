297b6871332d0f049a2b854ddbd2c92369add752
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-12 23:41:19 +0800

    实现给按钮附着任务数据，并根据任务数据自动显示任务名，按下按钮后修改属性值

--- Source/AIGame/TaskButtonWidget.cpp
+++ Source/AIGame/TaskButtonWidget.cpp
@@ -2,4 +2,42 @@
 
 
 #include "TaskButtonWidget.h"
+#include "ScheduleTaskDataAsset.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "GameDataSubsystem.h"
 
+void UTaskButtonWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	// 根据任务数据自动设置任务名称
+	if (TaskName)
+	{
+		TaskName->SetText(TaskDataAsset->TaskName);
+	}
+	// 订阅任务按钮的点击事件
+	if (TaskButton)
+	{
+		TaskButton->OnClicked.AddDynamic(this, &UTaskButtonWidget::OnTaskButtonClicked);
+	}
+}
+
+void UTaskButtonWidget::OnTaskButtonClicked()
+{
+	// 检查任务数据非空
+	if (not TaskDataAsset) return;
+	
+	// 获取 Subsystem
+	UGameDataSubsystem* Subsystem = GetGameInstance()->GetSubsystem<UGameDataSubsystem>();
+	if (Subsystem)
+	{
+		// 遍历任务数据所有结果，并调用子系统接口函数修改属性
+		for (const auto& Modifier : TaskDataAsset->AttributeModifiers)
+		{
+			Subsystem->ModifyAttribute(Modifier.Key, Modifier.Value);
+		}
+		
+		// TODO: 播放音效之类的
+	}
+}
--- Source/AIGame/TaskButtonWidget.h
+++ Source/AIGame/TaskButtonWidget.h
@@ -6,6 +6,9 @@
 #include "Blueprint/UserWidget.h"
 #include "TaskButtonWidget.generated.h"
 
+class UScheduleTaskDataAsset;
+class UButton;
+class UTextBlock;
 /**
  * 
  */
@@ -13,4 +16,24 @@ UCLASS()
 class AIGAME_API UTaskButtonWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	// 绑定任务数据
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "任务数据")
+	TObjectPtr<UScheduleTaskDataAsset> TaskDataAsset;
+	
+protected:
+	// 绑定按钮控件
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+	UButton* TaskButton;
+	
+	// 绑定任务名称
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+	UTextBlock* TaskName;
+	
+	// 重写构造函数
+	virtual void NativeConstruct() override;
+	
+	// 绑定按钮的回调函数
+	UFUNCTION()
+	void OnTaskButtonClicked();
 };

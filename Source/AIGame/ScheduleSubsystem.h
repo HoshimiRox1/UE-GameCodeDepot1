ef4b9a5b27d796c83c57922693c42ad2ff519c48
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-17 13:21:51 +0800

    养成流程逻辑

---
+++ Source/AIGame/ScheduleSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ScheduleSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIGAME_API UScheduleSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};

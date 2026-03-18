28f645f61a7930f23e8fc566256a225f17d29b57
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-18 17:17:02 +0800

    实现数据子系统的相关接口

--- Source/AIGame/GameDataSubsystem.cpp
+++ Source/AIGame/GameDataSubsystem.cpp
@@ -7,70 +7,57 @@
 void UGameDataSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
-	
-	// 初始化默认属性数值
-	PlayerStats.Add(EPlayerAttribute::Decision, 0.0f);
-	PlayerStats.Add(EPlayerAttribute::Social, 0.0f);
-	PlayerStats.Add(EPlayerAttribute::Empathy, 0.0f);
-	PlayerStats.Add(EPlayerAttribute::Cash, 100.0f);
-	PlayerStats.Add(EPlayerAttribute::DailyMood, 1.0f);
 }
 
-void UGameDataSubsystem::ModifyAttribute(EPlayerAttribute Type, float Amount)
+int32 UGameDataSubsystem::GetAttributeValue(EAttributeType Type) const
 {
-	if (PlayerStats.Contains(Type))
+	// 根据Type返回对应属性
+	switch (Type)
 	{
-		PlayerStats[Type] += Amount;
-		
-		// 将心情值钳制在0-1
-		if (Type == EPlayerAttribute::DailyMood)
+	case EAttributeType::Decision:	return GameState.DecisionValue;
+	case EAttributeType::Social:	return GameState.SocialValue;
+	case EAttributeType::Empathy:	return GameState.EmpathyValue;
+	default:						return 0;
+	}
+}
+
+void UGameDataSubsystem::ApplyTaskReward(const FTaskReward& Reward)
+{
+	// 遍历任务奖励内的属性奖励，根据类型修改对应属性值
+	for (const auto& AR : Reward.AttributeRewards)
+	{
+		switch (AR.Attribute)
 		{
-			PlayerStats[Type] = FMath::Clamp(PlayerStats[Type], 0.0f, 1.0f);
+		case EAttributeType::Decision:
+			GameState.DecisionValue += AR.Value;
+			break;
+		case  EAttributeType::Empathy:
+			GameState.EmpathyValue += AR.Value;
+			break;
+		case EAttributeType::Social:
+			GameState.SocialValue += AR.Value;
+			break;
 		}
 		
-		// 广播属性修改
-		OnAttributeChanged.Broadcast(Type, PlayerStats[Type]);
-	}
-}
-
-void UGameDataSubsystem::ResetDailyStats()
-{
-	ModifyAttribute(EPlayerAttribute::DailyMood, 1.0f - PlayerStats[EPlayerAttribute::DailyMood]);
-	
-}
-
-float UGameDataSubsystem::GetAttributeValue(EPlayerAttribute Attribute) const
-{
-	return PlayerStats.Contains(Attribute) ? PlayerStats[Attribute] : 0.0f;
-}
-
-void UGameDataSubsystem::SelectTask(UScheduleTaskDataAsset* NewTask)
-{
-	// TODO: 任务事件状态切换为『选中』
-}
-
-void UGameDataSubsystem::ComfirmSelectedTask()
-{
-	// TODO: 锁定选中任务，其他任务变灰禁用
-}
-
-void UGameDataSubsystem::ExecuteToday()
-{
-	if (not SelectedTask) return;
-	
-	// 修改属性
-	for (const auto& Modifier : SelectedTask->AttributeModifiers)
-	{
-		ModifyAttribute(Modifier.Key, Modifier.Value);
+		// 广播：属性变化
+		OnAttributeChanged.Broadcast(AR.Attribute, GetAttributeValue(AR.Attribute));
 	}
 	
-	// 清空选中任务
-	SelectedTask = nullptr;
+	// 修改其他单项属性
+	GameState.ThoughtPoints += Reward.ThoughtPointReward;
+	GameState.AnnieRapport += Reward.AnnieReward;
+	GameState.Cash -= Reward.CashCost;
 	
-	// 当前日子++
-	CurrentDate++;
-	
-	// 广播事件——新的一天，刷新ScheduleMain
-	// TODO:但其实这里有BUG，就是ModifyAttribute()本来就会广播导致UI刷新，这里广播其实没太大用处，后面重整逻辑
-	OnNewDayStarted.Broadcast();
+	// 广播：其他属性变化
+	if (Reward.ThoughtPointReward != 0) OnThoughtPointsChanged.Broadcast(GameState.ThoughtPoints);
+	if (Reward.AnnieReward != 0) OnAnnieRapportChanged.Broadcast(GameState.AnnieRapport);
+	if (Reward.CashCost != 0) OnCashChanged.Broadcast(GameState.Cash);
 }
+
+void UGameDataSubsystem::AdvanceDate()
+{
+	GameState.CurrentDate.AdvanceDay();
+	OnDateChanged.Broadcast(GameState.CurrentDate);
+}
+
+

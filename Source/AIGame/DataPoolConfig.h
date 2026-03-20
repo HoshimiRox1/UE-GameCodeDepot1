3c744fa009691d6d1d3f995bbd5cdd4afc1174c4
HoshimiRox1 <166687527+HoshimiRox1@users.noreply.github.com>
2026-03-20 09:21:09 +0800

    任务池配置文件

---
+++ Source/AIGame/DataPoolConfig.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataPoolConfig.generated.h"
+
+class UScheduleTaskDataAsset;
+
+/**
+ * 
+ */
+UCLASS()
+class AIGAME_API UDataPoolConfig : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "任务池")
+	TArray<UScheduleTaskDataAsset*> AlphaTasks;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "任务池")
+	TArray<UScheduleTaskDataAsset*> BetaTasks;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "任务池")
+	TArray<UScheduleTaskDataAsset*> GammaTasks;
+};
